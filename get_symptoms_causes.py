6f28b8f7cc817aa2ab580a9eea71bbaabc5d0422
Michael Fashola <michaelfashola@gmail.com>
2024-02-14 09:28:34 -0400

    fetch symptoms and causes data

---
+++ get_symptoms_causes.py
@@ -0,0 +1,35 @@
+# author: Michael Fashola
+
+import string
+import requests
+import pandas  as pd
+import json
+from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
+
+# All letters
+letters = list(string.ascii_uppercase)
+
+# Scrape disease content pages
+for letter in letters:
+    content_list = []
+    df = pd.read_json("pages/base_{}.json".format(letter))
+    for ix, row in df.iterrows():
+        if row["name"].startswith(letter):
+            print("Fetching content for {}".format(row["name"]))
+            response = requests.get(row["href"])
+            soup = BeautifulSoup(response.text, features="html.parser")
+            nodeList = soup.find_all("div", "row")
+            if (len(nodeList) >= 3):
+                content_list.append(nodeList[3].get_text())
+            else:
+                content_list.append(None)
+            sleep(randint(1,5))
+        else:
+            content_list.append(None)
+
+    # Output final file for letter
+    df["content"] = content_list
+    df.to_json("disease_data_{}.json".format(letter), orient="records", force_ascii=False)
+
---
+++ tsv2json.py
@@ -0,0 +1,30 @@
+# @author: GeeksforGeeks (https://www.geeksforgeeks.org/python-tsv-conversion-to-json/)
+
+import json 
+  
+def tsv2json(input_file,output_file): 
+    arr = [] 
+    file = open(input_file, 'r') 
+    a = file.readline() 
+      
+    # The first line consist of headings of the record  
+    # so we will store it in an array and move to  
+    # next line in input_file. 
+    titles = [t.strip() for t in a.split('\t')] 
+    for line in file: 
+        d = {} 
+        for t, f in zip(titles, line.split('\t')):
+          # Convert each row into dictionary with keys as titles
+          # we will use strip to remove '\n'. 
+            d[t] = f.strip()
+        arr.append(d) 
+          
+        # we will append all the individual dictionaires into list  
+        # and dump into file. 
+    with open(output_file, 'w', encoding='utf-8') as output_file: 
+        output_file.write(json.dumps(arr, indent=4)) 
+  
+# Driver Code 
+input_filename = 'pages/disease_data.tsv'
+output_filename = 'pages/disease_data_A.json'
+tsv2json(input_filename,output_filename)
